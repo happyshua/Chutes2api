0fe12f6dd7787138431fd5d2914d655476de3ffe
happyshua <167332648+happyshua@users.noreply.github.com>
2025-02-28 22:37:18 +0800

    Create deno.js

---
+++ deno.js
@@ -0,0 +1,10 @@
+{
+  "tasks": {
+    "start": "deno run --allow-net --allow-env --allow-read server.ts"
+  },
+  "imports": {
+    "oak": "https://deno.land/x/oak@v12.6.1/mod.ts",
+    "cors": "https://deno.land/x/cors@v1.2.2/mod.ts",
+    "uuid": "https://deno.land/std@0.220.1/uuid/mod.ts"
+  }
+}
