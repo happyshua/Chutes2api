f1f928ba1223f47bb9c3b4912fbf229e7ff3cdbb
happyshua <167332648+happyshua@users.noreply.github.com>
2025-02-28 22:41:51 +0800

    更新 deno.js

--- deno.js
+++ deno.js
@@ -1,10 +1,10 @@
+// deno.json - Configuration file for the Deno project
 {
   "tasks": {
-    "start": "deno run --allow-net --allow-env --allow-read server.ts"
+    "start": "deno run --allow-net --allow-env server.ts"
   },
   "imports": {
     "oak": "https://deno.land/x/oak@v12.6.1/mod.ts",
-    "cors": "https://deno.land/x/cors@v1.2.2/mod.ts",
-    "uuid": "https://deno.land/std@0.220.1/uuid/mod.ts"
+    "cors": "https://deno.land/x/cors@v1.2.2/mod.ts"
   }
 }
